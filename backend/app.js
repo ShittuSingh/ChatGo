d24d0aacd23dff8749a158606c5daa59f3af83e9
RAJ SINGH <rajsingh@Shitanshus-MacBook-Pro.local>
2025-11-17 13:54:16 +0530

    Backend changes

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
-const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Connect DB and start server
